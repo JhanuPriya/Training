d0d071c27930b4a00c7c2bada2ebb4405b997aeb
JhanuPriya.S <JhanuPriya.S@EXIND3020-32.allfon.in>
2022-04-20 18:10:37 +0530

    lambda

---
+++ Java8_Lambada/src/com/training/App2.java
@@ -0,0 +1,16 @@
+package com.training;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class App2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List<String> names= Arrays.asList("India","Srilanka","Indonesia","Bangulore");
+	//	names.forEach(value-> System.out.println(value));
+		//method referance
+		names.forEach( System.out::println);
+	}
+
+}
