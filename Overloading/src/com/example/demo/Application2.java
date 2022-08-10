b467292bcf336d37c82d89c3b286089f2ac44a3b
JhanuPriya.S <JhanuPriya.S@EXIND3020-32.allfon.in>
2022-08-10 12:46:49 +0530

    ss

---
+++ Overloading/src/com/example/demo/Application2.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+import java.util.*;
+
+public class Application2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc = new Scanner(System.in);
+System.out.println("Enter the Name");
+String name= sc.next();
+   System.out.println("enter the farenheit temp");
+   double farenTemp =sc.nextDouble();
+    Converter convObj = new Converter();
+	 System.out.println (convObj.convert(name));
+	System.out.println("Celsius Temp:="+convObj.convert(farenTemp));
+	System.out.println("enetr the amount");
+	double amount = sc.nextDouble();
+	System.out.println("enter the current to convert 1used 2 euro");
+	int frm =sc.nextInt();
+	System.out.println(convObj.convert(amount,frm));
+	sc.close();
+
+	}
+
+}
